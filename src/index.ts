29850bb365d8afe4f6b99d3f3b0fa5686a8d2701
Krunal Shah <shahkrunal7091@gmail.com>
2021-08-06 11:04:05 +0530

    fix: cors issue

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,11 @@ import express from 'express'
 import { graphqlHTTP } from 'express-graphql'
 import { schema } from './schema'
 import { context } from './context'
+import cors from 'cors'
+
 const app = express()
+
+app.use(cors())
 
 app.use(
   '/graphql',
